1994ce95a486c68b2b2de0c7e298b77130d8615b
brynnke <100329799+brynnke@users.noreply.github.com>
2022-05-08 18:08:07 -0500

    roots changed and working on adding projects

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+function projectClicked(input) {
+    var input = input;
+
+    if (input == "challenge-1-refactor") {
+        window.open("https://github.com/brynnke/challengeone/");
+    } else if (input == "run-buddy") {
+        window.open("https://github.com/brynnke/runbuddy/");
+    } else if (input == 'password-generator') {
+        window.open("https://brynnke.github.io/passwordgen/");
+    } else if (input == 'web-api-code-quiz') {
+        window.open("https://brynnke.github.io/jsquiz/");
+    } else if (input == "work-day-scheduler"){
+        window.open("https://brynnke.github.io/work-day-sched/");
+    } else if (input == "project 1"){
+        window.open ("https://omarwright07.github.io/music-guru/");
+    } else {
+        return true; 
+    }
+};

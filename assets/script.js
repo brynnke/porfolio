6f603b8bcd3ff776640f437d9a2b43a06ec8e2d2
brynnke <100329799+brynnke@users.noreply.github.com>
2022-05-08 18:12:00 -0500

    pretty much have what I want so far, made minor changes for a more visually appeally site. Have the index updated and style'

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 function projectClicked(input) {
     var input = input;
 
-    if (input == "challenge-1-refactor") {
+    if (input == "challenge-1") {
         window.open("https://github.com/brynnke/challengeone/");
     } else if (input == "run-buddy") {
         window.open("https://github.com/brynnke/runbuddy/");
     } else if (input == 'password-generator') {
         window.open("https://brynnke.github.io/passwordgen/");
-    } else if (input == 'web-api-code-quiz') {
+    } else if (input == 'api-code-quiz') {
         window.open("https://brynnke.github.io/jsquiz/");
     } else if (input == "work-day-scheduler"){
         window.open("https://brynnke.github.io/work-day-sched/");
